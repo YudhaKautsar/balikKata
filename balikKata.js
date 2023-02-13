838bb116b3acbb5bb51d13a312cfef295bf47141
Yudha Kautsar <muhammad.kautsar@iconpln.co.id>
2023-02-13 21:46:47 +0700

    first commit

---
+++ balikKata.js
@@ -0,0 +1,15 @@
+
+function balikKata(kata) {
+    let newString = ''
+    for (i = kata.length - 1; i >= 0; i--) {
+        newString += kata[i];
+    }
+
+    return newString
+}
+    
+// testCase
+console.log(balikKata("Niomic!"))    
+console.log(balikKata("JavaScript"))  
+console.log(balikKata("alohahola"))  
+console.log(balikKata("Jawa_Barat"))  
